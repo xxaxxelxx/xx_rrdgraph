a5942aee17b536fed3e6521150bd38adb8d96963
xxaxxelxx <x@axxel.net>
2015-12-03 12:07:02 +0100

    updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,10 @@
 CUSTOMER=$1
 
 # checking the environment
-LINKED_CONTAINER=$(env | grep '_ENV_' | head -n 1 | awk '{print $1}' | sed 's/_ENV_.*//')
-LB_HOST="$(cat /etc/hosts | grep -iw ${LINKED_CONTAINER} | awk '{print $1}')"
+#LINKED_CONTAINER=$(env | grep '_ENV_' | head -n 1 | awk '{print $1}' | sed 's/_ENV_.*//')
+#LB_HOST="$(cat /etc/hosts | grep -iw ${LINKED_CONTAINER} | awk '{print $1}')"
 
-
-
-./collect.sh $LB_HOST $RRD_LOOP $CUSTOMER
+./paint.sh $LB_HOST $RRD_LOOP $CUSTOMER
 
 #bash
 
