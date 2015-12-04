50a5796925836434d192afd0e8434a7eff4e3c26
xxaxxelxx <x@axxel.net>
2015-12-04 12:34:25 +0100

    updated

--- paint.sh
+++ paint.sh
@@ -85,7 +85,7 @@ else
 ##		    CDEFLINE="$CDEFLINE CDEF:${MOUNT_ID}show=${MOUNT_ID}test,$NUM,+"
 #		fi
 		echo "area lining simulcats..."
-		AREALINE="$AREALINE AREA:${MOUNT_ID}test#${A_COLOR_LIGHT[$NUM]}:${MOUNT_PRINT}:STACK VDEF:${MOUNT_ID}max=${MOUNT_ID}test,MAXIMUM GPRINT:${MOUNT_ID}max:%6.0lf&#32;%s\\c"
+		AREALINE="$AREALINE AREA:${MOUNT_ID}test#${A_COLOR_LIGHT[$NUM]}:${MOUNT_PRINT} VDEF:${MOUNT_ID}max=${MOUNT_ID}test,MAXIMUM GPRINT:${MOUNT_ID}max:%6.0lf&#32;%s\\c"
 		OUTLINE="$OUTLINE LINE1:${MOUNT_ID}test#${A_COLOR_DARK[$NUM]}::STACK"
 	    else
 		    echo "lining channels..."
